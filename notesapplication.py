de8febfe523bfe8a16bcf03e0f56484784d869ae
Loice-Andia <loiceandia@gmail.com>
2016-06-16 11:52:25 +0300

    Changed all functions from print to return

--- notesapplication.py
+++ notesapplication.py
@@ -12,25 +12,27 @@ class NotesApplication(object):
 
     def notes(self):
         for note_id in range(len(self.note_list)):
-            print "Note ID {} \n {}\n  By author {}".format(note_id,self.note_list[note_id],self.author)
+            return note_id,self.note_list[note_id],self.author
         
     def get_note(self, note_id):
-    	print str(self.note_list[note_id])
+    	return str(self.note_list[note_id])
     
     def search(self, search_text):
         for content in range(len(self.note_list)):
             if search_text in self.note_list[content]:
                 
-                print "Search for ",search_text, "found in ", self.note_list[content]            
+                return search_text, self.note_list[content]            
         
     def edit_notes(self, note_id, new_content):
         self.note_list[note_id] = new_content
-        print self.note_list[note_id]
+        return self.note_list[note_id]
 
 lolo_notes = NotesApplication("Lolo")
+
 lolo_notes.create("Lolo is fun")
 lolo_notes.create("She talks alot")
-lolo_notes.notes()
-lolo_notes.get_note(1)
-lolo_notes.search("lo")
-lolo_notes.edit_notes(0, "Lolo is boring")
+
+print lolo_notes.notes()
+print lolo_notes.get_note(1)
+print lolo_notes.search("lo")
+print lolo_notes.edit_notes(0, "Lolo is boring")
